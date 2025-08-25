25fb807569ec911e6d24067979eb3e951723f3da
Tom Malone <tomjmalone@gmail.com>
2025-08-25 10:51:52 +0100

    adding clipboard

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,8 +1,9 @@
+require("config.lazy")
+
 vim.opt.expandtab = true
 vim.opt.tabstop = 2
 vim.opt.softtabstop = 2
 vim.opt.shiftwidth = 2
-
-require("config.lazy")
+vim.opt.clipboard='unnamedplus'
 
 vim.cmd.colorscheme 'nord'
