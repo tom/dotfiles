eddb0661cc1841ae61a5ecb786cf5ab2f2e48dbd
Tom Malone <tomjmalone@gmail.com>
2026-05-02 22:46:57 +0100

    More tweaks

--- fish/.config/fish/conf.d/homebrew.fish
+++ fish/.config/fish/conf.d/homebrew.fish
@@ -1,1 +1,1 @@
-set PATH "/opt/homebrew/bin" "/opt/homebrew/sbin" $PATH
+set PATH /opt/homebrew/bin /opt/homebrew/sbin /opt/homebrew/opt/ruby/bin $PATH
--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,3 +1,12 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
 end
+
+
+# BEGIN opam configuration
+# This is useful if you're using opam as it adds:
+#   - the correct directories to the PATH
+#   - auto-completion for the opam binary
+# This section can be safely removed at any time if needed.
+test -r '/Users/tom/.opam/opam-init/init.fish' && source '/Users/tom/.opam/opam-init/init.fish' > /dev/null 2> /dev/null; or true
+# END opam configuration
